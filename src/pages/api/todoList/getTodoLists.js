a4e33b2c6b96bb1bae04bebf7f45a25925f4278d
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-10-21 15:25:54 -0400

    filter for not deleted

--- src/pages/api/todoList/getTodoLists.js
+++ src/pages/api/todoList/getTodoLists.js
@@ -4,7 +4,7 @@ import List from "../../../models/todolist"
 export default async function getTodoLists(req, res) {
   try {
     await connectDB()
-    const lists = await List.find()
+    const lists = await List.find({ deleted: false }) //get not deleted lists
     res.json(lists)
   } catch (error) {
     res.json({ msg: error.message })
