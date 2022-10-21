96bd5f689fd2f917f2e692c261ba0ddc5c704cf8
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-10-21 15:32:50 -0400

    sort by newest first

--- src/pages/api/todoList/getTodoLists.js
+++ src/pages/api/todoList/getTodoLists.js
@@ -4,7 +4,7 @@ import List from "../../../models/todolist"
 export default async function getTodoLists(req, res) {
   try {
     await connectDB()
-    const lists = await List.find({ deleted: false }) //get not deleted lists
+    const lists = await List.find({ deleted: false }).sort({ _id: "desc" }) //get not deleted lists and sort by newest first
     res.json(lists)
   } catch (error) {
     res.json({ msg: error.message })
