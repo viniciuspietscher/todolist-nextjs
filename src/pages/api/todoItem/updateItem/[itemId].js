e4a999e6ffc66a162251f5aeb2581fe358f69053
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-10-23 23:20:56 -0400

    add update/mark completed item endpoint

---
+++ src/pages/api/todoItem/updateItem/[itemId].js
@@ -0,0 +1,19 @@
+import connectDB from "../../../../lib/mongodb"
+import Item from "../../../../models/todoitem"
+
+export default async function updateTodoItem(req, res) {
+  const { itemId } = req.query
+  try {
+    await connectDB()
+    const item = await Item.findById(itemId)
+    if (!item) {
+      res.status(400).json({ msg: "Item does not exists" })
+      return
+    }
+    await Item.findByIdAndUpdate(itemId, { completed: true })
+    const completedItem = await Item.findById(itemId)
+    res.status(200).json({ completedItem })
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+}
